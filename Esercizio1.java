86c92ad9e2225a6a4f144f371aa81eec2f70fd64
FabioMarras <fabio.marras@yahoo.com>
2023-10-03 15:32:15 +0200

    Fix Es.1

--- Esercizio1.java
+++ Esercizio1.java
@@ -1,4 +1,4 @@
-public class Es1 {
+public class Esercizio1 {
     public static void main(String[] args) {
         String x = "ciao";
         String y = "ciaoo";
@@ -12,6 +12,16 @@ public class Es1 {
             System.out.println(true);;
         } else {
             System.out.println(false);
+        }
+
+        int anno = 2024;
+        int anno2 = 2025;
+
+//        if (anno % 4 == 0 || 100 == 0 && 400 == 0) {
+            if (anno % 100 == 0 && 400 == 0 || anno % 4 == 0) {
+            System.out.println("anno bisestile");
+        } else {
+            System.out.println("anno NON bisestile");
         }
     }
 }

164dc5f4c5766f66d1cd4f3da5f5331ea328b24a
FabioMarras <fabio.marras@yahoo.com>
2023-10-03 13:58:24 +0200

    Initial commit

---
+++ Es1.java
@@ -0,0 +1,17 @@
+public class Es1 {
+    public static void main(String[] args) {
+        String x = "ciao";
+        String y = "ciaoo";
+
+        if (x.length() % 2 == 0) {
+            System.out.println(true);;
+        } else {
+            System.out.println(false);
+        }
+        if (y.length() % 2 == 0) {
+            System.out.println(true);;
+        } else {
+            System.out.println(false);
+        }
+    }
+}

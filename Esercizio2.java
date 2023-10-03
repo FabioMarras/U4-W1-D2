de744c8ed0131ea6d1f73fe53def459ded85b9ac
FabioMarras <fabio.marras@yahoo.com>
2023-10-03 14:04:38 +0200

    Add Es. 2

---
+++ Esercizio2.java
@@ -0,0 +1,22 @@
+public class Esercizio2 {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 5;
+        switch (y){
+            case 1: {
+                System.out.println("uno");
+                break;
+            }
+            case 2: {
+                System.out.println("due");
+                break;
+            }
+            case 3: {
+                System.out.println("tre");
+                break;
+            }
+            default:
+                System.out.println("errore");
+        }
+    }
+}

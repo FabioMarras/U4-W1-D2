a00002fef9f0831d3d683b022aa23d9a47e70f2f
FabioMarras <fabio.marras@yahoo.com>
2023-10-03 14:37:49 +0200

    Add Es.3

---
+++ Esercizio3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Esercizio3 {
+    public static void main(String[] args) {
+
+            Scanner input = new Scanner(System.in);
+        System.out.println("Scrivi");
+        String uno = input.nextLine();
+        while (!uno.equals(":q")) {
+            System.out.println("Per finire scrivi :q");
+            uno = input.nextLine();
+        }
+            input.close();
+    }
+}
